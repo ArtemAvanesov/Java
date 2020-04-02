1e5827848af5b437f43b44aa65555cc6995853cb
ArtemAvanesov <artemavanecov@mail.ru>
2020-04-02 13:40:14 +0300

    Update lab2

--- 2/Main.java
+++ 2/Main.java
@@ -6,7 +6,7 @@ public class Main {
         boolean flag = false;
         Scanner in = new Scanner(System.in);
         try {
-            System.out.print("Введите число: ");
+            System.out.print("Enter the num: ");
             a = in.nextInt();
             if (a <= 0) throw  new Exception();
            for(int i = 2; i <= Math.sqrt(a); i++)
@@ -15,13 +15,13 @@ public class Main {
            }
             if(a == 2) flag = true;
         if(flag)
-            System.out.println("Число " + a +" - составное");
+            System.out.println("Num " + a +" is compound number");
             else
-            System.out.println("Число " + a +" - простое");
+            System.out.println("Число " + a +" is prime number");
         }
-        // Отлов исключений
+        // Catch exceptions
         catch (Exception e) {
-            System.out.println("Вы ввели недопустимое значение!");
+            System.out.println("Incorrect data!");
         }
     }
 }
