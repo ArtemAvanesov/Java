992970f2fe402e960dfecc582362cbd47a9d99e6
ArtemAvanesov <artemavanecov@mail.ru>
2020-04-04 21:48:54 +0300

    Update lab4

--- 4/Main.java
+++ 4/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         int k = 1;
 
-        // Заполнение массива
+        // Array filling
         int[][] array = new int[7][7];
         for(int i = 1; i <= 7; i++)
             if(i%2 == 0)
@@ -13,7 +13,7 @@ public class Main {
                 for(int j = 7; j >= 1; j--)
                 {array[i-1][j-1]=k; k++;}
 
-        // Вывод массива
+        // Return array
         for(int i = 1; i <= 7; i++)
         {System.out.println();
             for(int j = 1; j <= 7; j++)
