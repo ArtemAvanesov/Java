8c20088aea64de56f4a30ceb1a7c455cf1b6f848
ArtemAvanesov <artemavanecov@mail.ru>
2020-04-05 18:42:34 +0300

    Update lab5

--- 5/Main.java
+++ 5/Main.java
@@ -1,30 +1,25 @@
 public class Main {
 
-
     static byte b = 8;
     static short sh = 16;
     static int it = 32;
     static long l = 64;
-
     static float fl = 3.2f;
     static double db = 6.4;
-
     static boolean bool = true;
     static boolean bool2 = false;
-
     static  String st = "String";
     static  char c = 'd';
 
     public static void main(String[] args) {
+
         System.out.println("byte " + b);
         System.out.println("short " + sh);
         System.out.println("int " + it);
         System.out.println("long " + l);
-
         System.out.println("float " + fl);
         System.out.println("double " + db);
         System.out.println("boolean " + bool);
-
         System.out.println("String " + st);
         System.out.println("char " + c);
     }
