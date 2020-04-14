626288091a9472a6fd8a22225217ad7bfbf32874
ArtemAvanesov <artemavanecov@mail.ru>
2020-04-14 12:38:38 +0300

    Update lab14 (Main)

--- 14/Main.java
+++ 14/Main.java
@@ -1,15 +1,15 @@
-package com.company;
+﻿package com.company;
 
 public class Main {
     public  static  void main(String[] args) {
 
-        Cat cat1 = new Cat("Мурка", 5);
+        Cat cat1 = new Cat("Murka", 5);
         cat1.Info();
-        Cat cat2 = new Cat("Пушинка", 7);
+        Cat cat2 = new Cat("Fluff", 7);
         cat2.Info();
-        People people1 = new People("Василий", 23, cat1);
+        People people1 = new People("Basil", 23, cat1);
         people1.Info();
-        People people2 = new People("Петр", 20, cat2);
+        People people2 = new People("Petr", 20, cat2);
         people2.Info();
     }
 }
